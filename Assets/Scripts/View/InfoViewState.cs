0320cc6a82bc21544efe1b0a519dbb18eb61cf8f
mateus <mateus@marcopololearning.com>
2023-12-05 00:56:48 -0300

    fsm assemble in scene

---
+++ Assets/Scripts/View/InfoViewState.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace View
+{
+    public class InfoViewState : ViewBaseState
+    {
+        public override void OnEnter()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnExit()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
